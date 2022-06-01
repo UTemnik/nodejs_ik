197a1312c74f0329c46588cff1717599cda05f9d
Ulik <julia@datainlife.ru>
2022-06-01 12:41:46 +0300

    paths

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((request, response) => {
+const delayFunc = ms => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), ms);
+  })
+}
+
+const readFile = path => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data)
+      }
+    });
+  })
+}
+
+const server = http.createServer(async (request, response) => {
 
   switch (request.url) {
-    case '/students':
-      response.write('students');
+    case '/about': {
+      try {
+        const data = await readFile('pages/about.html');
+        response.write(data);
+        response.end();
+      } catch (e) {
+        response.write('error');
+        response.end();
+      }
       break;
-    case '/teachers':
-      response.write('teachers');
+    }
+    case '/home': {
+      await delayFunc(3000);
+      response.write('HOME');
+      response.end();
       break;
-    default:
+    }
+    default: {
       response.write('404 NOT FOUND');
+      response.end();
       break;
+    }
   }
-
-  response.end();
 });
 
 server.listen(3003);
